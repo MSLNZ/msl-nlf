cb4f50af3e0c90d3522411d9dbef940ba6149bf0
jborbely <joe.borbely@gmail.com>
2024-08-21 09:17:51 +1200

    create __about__.py

--- src/msl/nlf/__init__.py
+++ src/msl/nlf/__init__.py
@@ -13,18 +13,7 @@ from .models import PolynomialModel
 from .models import SineModel
 from .parameter import InputParameter
 from .parameter import InputParameters
-
-__author__ = 'Measurement Standards Laboratory of New Zealand'
-__copyright__ = '\xa9 2023, ' + __author__
-
-# The value of the version should follow the DLL version and Python bug fixes
-# {DLL major}.{DLL minor}.{Python patch, reset to 0 when DLL major or DLL minor changes}
-__version__ = '5.44.0.dev0'
-
-_v = re.search(r'(\d+)\.(\d+)\.(\d+)[.-]?(.*)', __version__).groups()
-
-version_info = namedtuple('version_info', 'major minor micro releaselevel')(int(_v[0]), int(_v[1]), int(_v[2]), _v[3])
-""":obj:`~collections.namedtuple`: Contains the version information as a (major, minor, micro, releaselevel) tuple."""
+from .__about__ import __version__, version_tuple
 
 __all__ = (
     'ConstantModel',
