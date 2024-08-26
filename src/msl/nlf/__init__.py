bad0a519a047910552bf0f6319b10c594d862923
jborbely <joe.borbely@gmail.com>
2024-08-26 15:43:58 +1200

    switch to mkdocs

--- src/msl/nlf/__init__.py
+++ src/msl/nlf/__init__.py
@@ -5,6 +5,6 @@ from __future__ import annotations
 from .__about__ import __version__, version_tuple
 from .datatypes import FitMethod, ResidualType
 from .loader import load
-from .model import LoadedModel, Model
+from .model import Model
 from .models import ConstantModel, ExponentialModel, GaussianModel, LinearModel, PolynomialModel, SineModel
-from .parameter import InputParameter, InputParameters
+from .parameters import InputParameter, InputParameters
