6214fb6cfd4ea47d44c84def74dc3fe2ad49c005
jborbely <joe.borbely@gmail.com>
2025-01-28 19:50:53 +1300

    update Copyright year to 2025

--- src/msl/nlf/__about__.py
+++ src/msl/nlf/__about__.py
@@ -5,4 +5,4 @@ from __future__ import annotations
 from ._version import __version__, version_tuple
 
 __author__: str = "Measurement Standards Laboratory of New Zealand"
-__copyright__: str = f"\xa9 2024, {__author__}"
+__copyright__: str = f"\xa9 2025, {__author__}"
